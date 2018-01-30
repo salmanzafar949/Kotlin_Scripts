a8abb1c473d925cdc64c1a407ac01d3ed57eb5de
Salman Zafar <salmanzafar949@gmail.com>
2018-01-30 13:13:23 +0500

    Maps explannation in kotlin

--- src/maps.kt
+++ src/maps.kt
@@ -1,4 +1,13 @@
+import java.util.*
+
 fun main(args : Array<String>)
 {
+    var map = TreeMap<String,Int>()
+    map["salman"] = 23
+    map["abc"]  = 40
 
+    for ((name,age) in map)
+    {
+        println("$name : $age")
+    }
 }
