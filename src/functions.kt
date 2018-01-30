0f76d387ee782db61bcd840d129284f7e96a5ce7
Salman Zafar <salmanzafar949@gmail.com>
2018-01-30 13:32:13 +0500

    useage of functions and implementation in kotlin

--- src/functions.kt
+++ src/functions.kt
@@ -1,4 +1,26 @@
 fun main(args: Array<String>)
 {
-
+    add(4,5)
+    println(divide(4,2))
+    println(mult(4,5))
+    println(max(4,5))
 }
+
+// simple function
+
+fun add(a : Int, b : Int)
+{
+  println(a + b)
+}
+
+// function with return values
+
+fun divide(a : Int, b : Int): Int {
+    return a/b
+}
+
+// Inline functions
+
+fun mult (a : Int, b : Int):Int=a*b
+
+fun max(a :Int, b : Int):Int = if(a>b){ a }else{ b }
