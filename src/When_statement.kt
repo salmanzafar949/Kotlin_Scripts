933b1e26adce6d29135391d48ff2c7aeac902fec
Salman Zafar <salmanzafar949@gmail.com>
2018-01-30 12:23:44 +0500

    When statement in kotlin and it's useage

---
+++ src/When_statement.kt
@@ -0,0 +1,12 @@
+fun main(args : Array<String>)
+{
+    val num : Int = 10;
+
+    when(num)
+    {
+        1 -> print("one")
+        2 -> print("three")
+        3 -> print("four")
+        else -> print("No Match Found")
+    }
+}
